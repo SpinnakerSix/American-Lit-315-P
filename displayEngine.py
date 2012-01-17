0639ac2d640dcf7e0c17fefa3b5b8dcc28fff7d0
Han Jing <hanqijing@gmail.com>
2012-01-16 17:42:56 -0800

    added control so you can switch between local or web APIs

--- displayEngine.py
+++ displayEngine.py
@@ -5,19 +5,17 @@ import urllib2 as urllib
 from PIL import Image, ImageTk
 from cStringIO import StringIO
 
+# my_url = 'http://aux.iconpedia.net/uploads/1440300232.png'
 
-def display(message, picture_link):
+def displayFromWebAPI(message, picture_url):
     print 'Displaying Coupon!'
+    root = Toplevel() #Tk() opens an interpreter, so Toplevel is what I want
 
-    root = Toplevel()
-    #root = Tk()
-    #my_url = 'http://aux.iconpedia.net/uploads/1440300232.png'
-    #img_file = urllib.urlopen(my_url)
-    #im = StringIO(img_file.read())
-    #image1 = Image.open(im)
-
+    img_file = urllib.urlopen(picture_url)
+    im = StringIO(img_file.read())
+    image1 = Image.open(im)
     #f = open(picture_link)
-    image1 = Image.open(picture_link)
+    #image1 = Image.open(picture_link)
     #image1 = picture_link
     #root.geometry('%dx%d' % (image1.size[0],image1.size[1]))
      
@@ -39,10 +37,31 @@ def display(message, picture_link):
     print image1
     print tkpi
     root.mainloop() #start the GUI
+
+def displayFromLocalFiles(message, picture_link):
+    root = Toplevel()
+    image1 = Image.open(picture_link)
+    # Convert the Image object into a TkPhoto object
+    tkpi = ImageTk.PhotoImage(image1)
+    label_image = Label(root, image=tkpi, text=message, font=("default",12),compound='top',width=image1.size[0], height=image1.size[1]+20)
+    label_image.image = tkpi # keep a reference or python Garbage collects this
+    label_image.pack()  # pack it in the display windowv
+	
+    width = image1.size[0]
+    height = image1.size[1]
+    root.geometry("400x200-25-0") # this control the size of the window and the position of it in relations to the screen
+    root.title("Your Reward!")
+
+    root.after(2000, root.destroy) # close the window after 2 seconds
+    root.mainloop() #start the GUI
  
-def displayCoupon(message, picture_link):
+def displayLocalCoupon(message, picture_link):
   #image1 = Image.open(picture_link)
-  thread.start_new_thread( display, (message, picture_link, ) )
+  thread.start_new_thread( displayFromLocalFiles, (message, picture_link, ) )
+
+def displayWebCoupon(message, picture_url):
+  picture_url = 'http://aux.iconpedia.net/uploads/1440300232.png'
+  thread.start_new_thread( displayFromWebAPI, (message, picture_url, ) )
 
 #print 'ladfadfadsadf'
 #display('Coupon Coupon Coupon', 'coke-coupon.png')
--- stateReader.py
+++ stateReader.py
@@ -1,6 +1,7 @@
 import util
 import displayEngine
 from gtktest import Image_Example
+from datetime import datetime
 
 # detects 3 things
 # achievement (passed a level)
@@ -9,45 +10,69 @@ from gtktest import Image_Example
 # CUSTOM #
 # GHOST HUNTER #
 
-from datetime import datetime
 path = '/home/innovision/Downloads/pacman_ingidio_demo/'
+
+# web or local. web gets picture from web, 
+# local displays picture from coupons/ folder
+API_CONTROL = 'local'
 
 class reader():
   def __init__(self):
      self.highScoresLimit = 1
      self.achievementsLimit = 1
-     self.closeCallsLimit = 2
+     self.closeCallsLimit = 3
      self.specialsLimit = 1
      
      # delay 20sec between displaying coupons
      self.bufferTime = 5
      self.lastCallTime = datetime.now()
-  
+      
+  # prevent multiple coupons from displaying
+  # at the same time
+
   # calculate the amount of time passed
   # uses to delay call time between coupons
+  def isBufferOver(self):
+     if self.timePassed() > self.bufferTime:
+       return True
+     #print 'No'
+     #print self.timePassed()
+     return False
+
   def timePassed(self):
      t =  datetime.now() - self.lastCallTime
      return t.seconds
 
   def highScore(self, currentScore, targetScore):
-     if (self.highScoresLimit > 0 and currentScore >= targetScore):
+     if (self.isBufferOver() and self.highScoresLimit > 0 and currentScore >= targetScore):
+       self.lastCallTime = datetime.now()
        self.highScoresLimit -= 1
        print 'Display NEW HIGH SCORE coupon!'
-       displayEngine.displayCoupon('New High Score!', path + 'coupons/coke-coupon.png')
+       if (API_CONTROL == 'web'):
+         displayEngine.displayWebCoupon('New High Score!', path + 'coupons/coke-coupon.png')
+       else:
+         displayEngine.displayLocalCoupon('New High Score!', path + 'coupons/coke-coupon.png')
        #Image_Example().threadMain()
  
   def victory(self, yesNo):
      if (self.achievementsLimit > 0 and yesNo):
        self.achievementsLimit -= 1
        print 'Display ACHIEVEMENT coupon!'
-       displayEngine.displayCoupon('Victory!', path + 'coupons/gamestop-coupon.jpeg')
+       if (API_CONTROL == 'web'):
+         displayEngine.displayWebCoupon('Victory!', path + 'coupons/gamestop-coupon.jpeg')
+       else:
+         displayEngine.displayLocalCoupon('Victory!', path + 'coupons/gamestop-coupon.jpeg')
 
   # accept a boolean isInSpecialState. If in special state, we call the coupon 
   def special(self, isInSpecialState, message='Special'):
-     if (self.specialsLimit > 0 and isInSpecialState):
+     if (self.isBufferOver() and self.specialsLimit > 0 and isInSpecialState):
+       self.lastCallTime = datetime.now()
        self.specialsLimit -= 1
        print 'Display SPECIAL coupon!'
-       displayEngine.displayCoupon(message, path + 'coupons/redbull-coupon.jpg')
+       if (API_CONTROL == 'web'):
+         displayEngine.displayWebCoupon(message, path + 'coupons/redbull-coupon.jpg')
+       else:
+         displayEngine.displayLocalCoupon(message, path + 'coupons/redbull-coupon.jpg')
    
   def isCloseCall(self, player, enemy):
     for e in enemy:
@@ -56,10 +81,13 @@ class reader():
     return True
        
   def closeCall(self, pacmanState, allGhostStates):
-    if (self.timePassed() > self.bufferTime and self.closeCallsLimit > 0 and self.isCloseCall(pacmanState, allGhostStates)):
+    if (self.isBufferOver() and self.closeCallsLimit > 0 and self.isCloseCall(pacmanState, allGhostStates)):
+      self.lastCallTime = datetime.now()
       self.closeCallsLimit -= 1
       print 'Display CLOSE CALL coupon!'
-      self.lastCallTime = datetime.now()
-      displayEngine.displayCoupon('Close Call!', path + 'coupons/popchips-coupon.jpg')
+      if (API_CONTROL == 'web'):
+        displayEngine.displayWebCoupon('Close Call!', path + 'coupons/popchips-coupon.jpg')
+      else:
+        displayEngine.displayLocalCoupon('Close Call!', path + 'coupons/popchips-coupon.jpg')
 
 
